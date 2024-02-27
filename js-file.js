b5f268e913ecdded7db77cbddede462228933e92
ogauthier <o.b.gauthier@gmail.com>
2024-02-27 00:22:53 -0500

    Add an option to customize the grid size

--- js-file.js
+++ js-file.js
@@ -1,21 +1,43 @@
-const container = document.querySelector('#container');
-numberOfSquare = 16;
-gridSize = numberOfSquare * numberOfSquare;
-widthOfSquare = (100/numberOfSquare).toLocaleString()+"%";
+createGrid();
+
+const btn = document.querySelector('#btn')
+btn.addEventListener('click',changeGrid)
 
 
-for (let i=0;i<gridSize;i++){
-    const square = document.createElement('div');
-    square.className= "square"
-    square.style.width = widthOfSquare;
-    container.appendChild(square);
-}
 
-const squares = document.querySelectorAll('.square');
-squares.forEach((square) => {
-    square.addEventListener('mouseenter', () => {
-    square.style.background = "white"
-      
+
+
+
+
+function game() {
+    const squares = document.querySelectorAll('.square');
+    squares.forEach((square) => {
+        square.addEventListener('mouseenter', () => {
+            square.style.background = "white";
+
+        });
     });
-  }); 
-
+}
+function createGrid(numberOfSquare = 16) {
+    const container = document.querySelector('#container');
+    gridSize = numberOfSquare * numberOfSquare;
+    widthOfSquare = (100 / numberOfSquare).toLocaleString() + "%";
+    for (let i = 0; i < gridSize; i++) {
+        const square = document.createElement('div');
+        square.className = "square";
+        square.style.width = widthOfSquare;
+        container.appendChild(square);
+    }
+    game()
+}
+function changeGrid(){
+    removeGrid();
+    let userGridsSize = prompt("Grid size");
+    createGrid(userGridsSize);
+}
+function removeGrid(){
+    const squares = document.querySelectorAll('.square');
+    squares.forEach((square) => {
+        square.remove()
+    }); 
+}
